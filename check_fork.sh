51cf6687530ce59687d69491f919459e91227bd8
Mark Nijjar <107886692+tt-mnijjar@users.noreply.github.com>
2025-04-10 11:16:54 -0400

    Update check_fork.sh

--- check_fork.sh
+++ check_fork.sh
@@ -92,7 +92,7 @@ main() {
 
     MAIN_BRANCH_NAME=$(cat $GITHUB_EVENT_PATH | jq -r '.pull_request.base.ref')
     FORK_BRANCH_NAME=$(cat $GITHUB_EVENT_PATH | jq -r '.pull_request.head.ref')
-    GIT_REPO_FULL_NAME=$(cat $GITHUB_EVENT_PATH | jq -r '.base.repo.full_name')
+    GIT_REPO_FULL_NAME=$(cat $GITHUB_EVENT_PATH | jq -r 'pull_request.base.repo.full_name')
     GITHUB_SERVER_URL="https://github.com"
     IS_FORK=$(cat $GITHUB_EVENT_PATH | jq -r '.head.repo.fork')
 
